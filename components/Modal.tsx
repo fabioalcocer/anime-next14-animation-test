3442e69bfd8d5623f524c528629ddc52bf0553ff
fabioalcocer <fabioalcocersejas17@gmail.com>
2023-11-27 00:00:08 -0400

    add animation to modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
+import { MotionDiv } from './MotionDiv'
 
 export default function Modal({
   children,
@@ -16,6 +17,11 @@ export default function Modal({
   const overlay = useRef(null)
   const wrapper = useRef(null)
   const router = useRouter()
+
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  }
 
   const onDismiss = useCallback(() => {
     router.back()
@@ -46,29 +52,40 @@ export default function Modal({
   }, [onKeyDown])
 
   return (
-    <div
-      ref={overlay}
-      className='fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60'
-      onClick={onClick}
+    <MotionDiv
+      variants={variants}
+      initial={'hidden'}
+      animate={'visible'}
+      transition={{
+        ease: 'ease',
+        duration: 0.3,
+      }}
+      viewport={{ amount: 0 }}
     >
       <div
-        ref={wrapper}
-        className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 p-6'
+        ref={overlay}
+        className='fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60'
+        onClick={onClick}
       >
         <div
-          className='absolute right-10 sm:right-16 top-10 ml-auto'
-          onClick={onDismiss}
+          ref={wrapper}
+          className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 p-6'
         >
-          <Image
-            src='/cross.svg'
-            alt='logo'
-            width={36}
-            height={36}
-            className='cursor-pointer'
-          />
+          <div
+            className='absolute right-10 sm:right-16 top-10 ml-auto'
+            onClick={onDismiss}
+          >
+            <Image
+              src='/cross.svg'
+              alt='logo'
+              width={36}
+              height={36}
+              className='cursor-pointer'
+            />
+          </div>
+          {children}
         </div>
-        {children}
       </div>
-    </div>
+    </MotionDiv>
   )
 }
